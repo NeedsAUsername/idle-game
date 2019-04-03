3cc999e56384855770080588f3ce2b100ce5af76
Kevin <kevinchi118@gmail.com>
2019-04-02 21:03:08 -0500

    adds dev update note

--- src/home/index.js
+++ src/home/index.js
@@ -7,7 +7,11 @@ class Home extends React.Component {
       <div>
         <h1 className="title">Welcome to Idle Game</h1>  
         <a href='/game' className="link">Play</a>
-        <p>Check out the Github source code <a href="https://github.com/NeedsAUsername/idle-game">here</a></p>
+        <p>Check out the Github source code <a href="https://github.com/NeedsAUsername/idle-game">here</a></p> 
+        <p>Characters from Maplestory</p> 
+        <ul>Dev version 2.10 4/2/2019</ul>  
+          <li>Adds CSS animations for enemy sprite walking/taking damage/dying</li> 
+          <li>Designs state management system for animations and hero attack speed</li>
         <ul>Dev version 2.00 3/31/2019</ul> 
           <li>Creates basic gameplay functionality</li>
           <li>Hero is able to attack and defeat the enemy, gain exp/gold, and level up</li>
