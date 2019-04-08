ec62811e23768fabb2e14e18a5522485dc796f68
Kevin <kevinchi118@gmail.com>
2019-04-07 22:36:09 -0500

    adds to dev notes

--- src/home/index.js
+++ src/home/index.js
@@ -9,6 +9,10 @@ class Home extends React.Component {
         <a href='/game' className="link">Play</a>
         <p>Check out the Github source code <a href="https://github.com/NeedsAUsername/idle-game">here</a></p> 
         <p>Characters from Maplestory</p> 
+        <ul>Dev version 2.20 4/7/2019</ul> 
+          <li>Adds CSS animations for character, attacking, and damage numbers</li> 
+          <li>Styles the layout for desktop and mobile view</li> 
+          <li>Adjusts damage and exp system</li>
         <ul>Dev version 2.10 4/2/2019</ul>  
           <li>Adds CSS animations for enemy sprite walking/taking damage/dying</li> 
           <li>Designs state management system for animations and hero attack speed</li>
