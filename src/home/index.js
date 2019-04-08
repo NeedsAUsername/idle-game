202830a656f4d1cef2b92af82c2d8177ea79aab6
Kevin <kevinchi118@gmail.com>
2019-04-07 22:45:05 -0500

    updates game title

--- src/home/index.js
+++ src/home/index.js
@@ -5,7 +5,7 @@ class Home extends React.Component {
   render () {
     return (
       <div>
-        <h1 className="title">Welcome to Idle Game</h1>  
+        <h1 className="title">Welcome to Idle Maple</h1>  
         <a href='/game' className="link">Play</a>
         <p>Check out the Github source code <a href="https://github.com/NeedsAUsername/idle-game">here</a></p> 
         <p>Characters from Maplestory</p> 
