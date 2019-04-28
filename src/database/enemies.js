dc9afd49f570503fd93bf9fd837bdf218ae18810
Kevin <kevinchi118@gmail.com>
2019-04-28 16:31:35 -0500

    adds more monsters

--- src/database/enemies.js
+++ src/database/enemies.js
@@ -47,6 +47,22 @@ const enemies = {
     exp: 8, 
     mesos: 15
   }, 
+  octopus: {
+    name: 'Octopus', 
+    animations: {
+      walking: 'octopus-walking', 
+      hurt: 'octopus-hurt', 
+      dying: 'octopus-dying'
+    }, 
+    currentAnimation: 'octopus-walking', 
+    damagesTaken: [], 
+    level: 12, 
+    totalHp: 200, 
+    currentHp: 200, 
+    autoAttack: 1, 
+    exp: 24, 
+    mesos: 30
+  },
   axeStump: {
     name: 'Axe Stump', 
     animations: {
@@ -62,6 +78,54 @@ const enemies = {
     autoAttack: 1, 
     exp: 30, 
     mesos: 40
+  }, 
+  kingSlime: {
+    name: 'King Slime', 
+    animations: {
+      walking: 'king-slime-walking', 
+      hurt: 'king-slime-hurt', 
+      dying: 'king-slime-dying'
+    }, 
+    currentAnimation: 'king-slime-walking', 
+    damagesTaken: [], 
+    level: 40, 
+    totalHp: 8000, 
+    currentHp: 8000,
+    autoAttack: 1, 
+    exp: 800, 
+    mesos: 500
+  }, 
+  stoneGolem: {
+    name: 'Stone Golem', 
+    animations: {
+      walking: 'stone-golem-walking', 
+      hurt: 'stone-golem-hurt', 
+      dying: 'stone-golem-dying'
+    }, 
+    currentAnimation: 'stone-golem-walking', 
+    damagesTaken: [], 
+    level: 55, 
+    totalHp: 4000, 
+    currentHp: 4000, 
+    autoAttack: 1, 
+    exp: 170, 
+    mesos: 300
+  }, 
+  redDrake: {
+    name: 'Red Drake', 
+    animations: {
+      walking: 'red-drake-walking', 
+      hurt: 'red-drake-hurt', 
+      dying: 'red-drake-dying'
+    }, 
+    currentAnimation: 'red-drake-walking', 
+    damagesTaken: [], 
+    level: 60, 
+    totalHp: 6000, 
+    currentHp: 6000, 
+    autoAttack: 1, 
+    exp: 220, 
+    mesos: 350
   }
 } 
 
