edae8936ae93f9af36076a2095f0e2ebfca9dd62
Kevin <kevinchi118@gmail.com>
2019-04-28 15:08:25 -0500

    adds to dev notes

--- src/home/index.js
+++ src/home/index.js
@@ -12,6 +12,7 @@ class Home extends React.Component {
         <ul>Dev version 4.00 4/28/2019</ul>
           <li>Adds multiple attacks for the hero, which required structural changes to the way damage and attack speed is calculated, 
             and how the damage numbers are output</li> 
+          <li>Uses flex-end property to keep all monsters rooted to the same bottom starting position</li>
         <ul>Dev version 3.10 4/21/2019</ul> 
           <li>Adds meso loot animation depending on amount dropped</li> 
           <li>Refactors health-bar into its own component, and stores small components in a components folder</li>
