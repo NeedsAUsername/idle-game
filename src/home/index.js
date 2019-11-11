a68064e12a6595e7a58bb2bf4fa79f2140f3993d
Kevin Chi <kevinchi118@gmail.com>
2019-11-11 11:42:59 -0600

    adds to dev notes

--- src/home/index.js
+++ src/home/index.js
@@ -9,6 +9,9 @@ class Home extends React.Component {
         <a href='/game' className="link">Play</a>
         <p>Check out the Github source code <a href="https://github.com/NeedsAUsername/idle-game">here</a></p> 
         <p>Characters from Maplestory</p> 
+        <ul>Dev version 5.00 11/11/2019</ul>
+          <li>Implements mana cost system</li>
+          <li>Bug fixes on level up message not showing, and not updating total hp/mp on level up</li>
         <ul>Dev version 4.00 4/28/2019</ul>
           <li>Adds multiple attacks for the hero, which required structural changes to the way damage and attack speed is calculated, 
             and how the damage numbers are output</li> 
