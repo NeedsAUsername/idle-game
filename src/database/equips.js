bd8a4e73a8c55b6fa59c685dfe6c58ac3767ea53
Kevin Chi <kevinchi118@gmail.com>
2019-11-26 11:02:02 -0600

    adds stars to db

---
+++ src/database/equips.js
@@ -0,0 +1,52 @@
+const stars = {
+  subi: {
+    cost: 0,
+    name: 'Subi',
+    className: 'subi'
+  },
+  wolbi: {
+    cost: 0,
+    name: 'Wolbi',
+    className: 'wolbi' 
+  },
+  mokbi: {
+    cost: 0,
+    name: 'Mokbi',
+    className: 'mokbi'
+  },
+  kumbi: {
+    cost: 0,
+    name: 'Kumbi',
+    className: 'kumbi'
+  },
+  tobi: {
+    cost: 0,
+    name: 'Tobi',
+    className: 'tobi'
+  },
+  steely: {
+    cost: 0,
+    name: 'Steely',
+    className: 'steely'
+  },
+  ilbi: {
+    cost: 0,
+    name: 'Ilbi',
+    className: 'ilbi'
+  },
+  crystalIlbi: {
+    cost: 0,
+    name: 'Crystal Ilbi',
+    className: 'crystal-ilbi'
+  },
+  snowball: {
+    cost: 0,
+    name: 'Snowball',
+    className: 'snowball'
+  }
+}
+
+const equips = {
+  stars
+}
+export default equips;
